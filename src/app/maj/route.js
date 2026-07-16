bfb353fb7783ea1ee0fbe7231fdb72d18c6966a9
hedi larbi <hedilarbi@hedis-MacBook-Air.local>
2026-07-16 21:26:51 +0100

    12

--- src/app/maj/route.js
+++ src/app/maj/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-const APK_URL = "https://expo.dev/artifacts/eas/OYhSBVv3lw921XlHd4Q8TSeAu-Bnm5NiXuSuWMZaUtE.apk";
+const APK_URL = "https://expo.dev/artifacts/eas/qBiaKoDbRDR2ENomqIMoQYPUO38a-KU1msF_CS1gXjs.apk";
 
 export function GET() {
   return NextResponse.redirect(APK_URL, { status: 302 });
